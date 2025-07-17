d37a06ff83b0080eb3845ab0d616e9093eefc9b8
thefool <122962215+thefool45@users.noreply.github.com>
2025-07-17 16:34:07 +0000

    Changes to be committed: 	modified:   .github/scripts/notify.py 	modified:   .github/workflows/sync.yml

--- .github/scripts/notify.py
+++ .github/scripts/notify.py
@@ -5,106 +5,134 @@ Send a rich Discord embed whenever a new nuclei-template YAML is added.
 – pretty colours that match severity
 """
 
-import os, sys, yaml, pathlib, requests, time
+import os, sys, yaml, pathlib, requests, time, random
+from urllib.parse import quote_plus
 
 # Hex colours → int for Discord embeds
 SEVERITY_COLOURS = {
-    "critical": 0xB71C1C,  # 🔥 Darker red
-    "high":     0xE53935,  # Bright red
-    "medium":   0xFB8C00,  # Orange
-    "low":      0x03A9F4,  # Light blue
-    "info":     0x90A4AE,  # Soft grey-blue
-    "unknown":  0xB0BEC5
+	"critical": 0xB71C1C,  # 🔥 Darker red
+	"high":     0xE53935,  # Bright red
+	"medium":   0xFB8C00,  # Orange
+	"low":      0x03A9F4,  # Light blue
+	"info":     0x90A4AE,  # Soft grey-blue
+	"unknown":  0xB0BEC5
 }
 
 SEVERITY_ICONS = {
-    "critical": "🛑",  # stop sign = urgent + highly visible
-    "high":     "🔴",  # red circle
-    "medium":   "🟠",  # orange
-    "low":      "🔵",  # blue
-    "info":     "⚪",  # white
-    "unknown":  "⚪"
+	"critical": "🛑",  # stop sign = urgent + highly visible
+	"high":     "🔴",  # red circle
+	"medium":   "🟠",  # orange
+	"low":      "🔵",  # blue
+	"info":     "⚪",  # white
+	"unknown":  "⚪"
 }
 
 def notify_discord(payload):
-    lst_webhook = [
-        os.environ["DISCORD_WEBHOOK"],
-        os.environ["DISCORD_WEBHOOK_FLYSEC"]
-    ]
-    for webhook in lst_webhook:
-        while True:
-            resp = requests.post(webhook, json=payload, timeout=10)
-            if resp.status_code == 429:
-                time.sleep(1)
-                continue
-            break
+	lst_webhook = [
+		os.getenv("DISCORD_WEBHOOK"),
+		os.getenv("DISCORD_WEBHOOK_FLYSEC")
+	]
+	for webhook in lst_webhook:
+		if webhook is None:
+			continue
+		while True:
+			resp = requests.post(webhook, json=payload, timeout=10)
+			if resp.status_code == 429:
+				time.sleep(1)
+				continue
+			break
+
+def get_shodan_query_count(query):
+	'''
+	count the total stats about a query, return a stat string
+	'''
+	all_total = -1
+	SHODAN_API = random.choice(os.getenv("SHODAN_KEY").split("\n"))
+	query = quote_plus(query)
+	url = f"https://api.shodan.io/shodan/host/count?query={query}&key={SHODAN_API}"
+	r = requests.get(url)
+	if r.status_code == 200:
+		d = r.json()
+		all_total = d.get("total")
+
+	return f"Total: {all_total}"
 
 def main(action, template_path):
-    template_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), template_path)
-    with open(template_path, "r", encoding="utf-8") as f:
-        data = yaml.safe_load(f)
+	template_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), template_path)
+	with open(template_path, "r", encoding="utf-8") as f:
+		data = yaml.safe_load(f)
 
-    # ── Extract info ────────────────────────────────────────────────────────────
-    info        = data.get("info", {})                       # new nesting
-    name        = pathlib.Path(template_path).name
-    severity    = str(info.get("severity", "unknown")).lower()
-    description = str(info.get("description", "No description"))
-    template_id = str(data.get("id", "unknown"))
-    root_path   = template_path.replace("\\", "/")
+	# ── Extract info ────────────────────────────────────────────────────────────
+	info        = data.get("info", {})                       # new nesting
+	name        = pathlib.Path(template_path).name
+	severity    = str(info.get("severity", "unknown")).lower()
+	description = str(info.get("description", "No description"))
+	template_id = str(data.get("id", "unknown"))
+	root_path   = template_path.replace("\\", "/")
 
-    reference = "\n".join(info.get("reference", [])).strip()
+	reference = info.get("reference", [])
+	if isinstance(reference, list):
+		reference = "\n".join(reference).strip()
 
-    # pick a colour; default = mid-grey
-    colour      = SEVERITY_COLOURS.get(severity, 0x95A5A6)
-    
-    shodan_query = info.get("metadata", {}).get("shodan-query", None)
-    fofa_query   = info.get("metadata", {}).get("fofa-query", None)
-    severity_icon = SEVERITY_ICONS.get(severity, "⚪")
+	# pick a colour; default = mid-grey
+	colour      = SEVERITY_COLOURS.get(severity, 0x95A5A6)
+	
+	shodan_query = info.get("metadata", {}).get("shodan-query", None)
+	fofa_query   = info.get("metadata", {}).get("fofa-query", None)
+	severity_icon = SEVERITY_ICONS.get(severity, "⚪")
 
-    fields = [
-        {"name": "Template ID", "value": template_id, "inline": True},
-        {"name": "Severity",    "value": f"{severity_icon} {severity.upper()}",    "inline": True},
-    ]
+	fields = [
+		{"name": "Template ID", "value": template_id, "inline": True},
+		{"name": "Severity",    "value": f"{severity_icon} {severity.upper()}",    "inline": True},
+	]
 
-    if shodan_query:
-        if isinstance(shodan_query, list):
-            shodan_query = "`\n- `".join(shodan_query)
-        fields.append({"name": "Shodan", "value": f"- `{shodan_query}`", "inline": False})
+	if shodan_query:
+		if isinstance(shodan_query, str):
+			shodan_query = [shodan_query]
 
-    if fofa_query:
-        if isinstance(fofa_query, list):
-            fofa_query = "`\n- `".join(fofa_query)
-        fields.append({"name": "Fofa", "value": f"- `{fofa_query}`", "inline": False})
+		new_query = []
+		for query in shodan_query:
+			query = query.strip()
+			total_query = get_shodan_query_count(query=query)
+			new_query.append(f"{query} - {total_query}")
 
-    if reference:
-        fields.append({"name": "Reference", "value": reference, "inline": False})
+		new_query = "`\n- `".join(new_query)
+		fields.append({"name": "Shodan", "value": f"- `{new_query}`", "inline": False})
 
-    root_path = root_path.replace("/workspaces/nuclei-templates/", "") \
-                   .replace("/home/runner/work/nuclei-templates/nuclei-templates/", "")
-    fields.append({"name": "Path", "value": "`" + root_path + "`", "inline": True})
-    
-    if action == "A":
-        title = f"🆕 `{name}`"
-    else:
-        # Temporary Disable Notify Modified Template
-        return
-        # title = f"✏️ `{name}`"
+	if fofa_query:
+		if isinstance(fofa_query, list):
+			fofa_query = "`\n- `".join(fofa_query)
+		fields.append({"name": "Fofa", "value": f"- `{fofa_query}`", "inline": False})
 
-    # ── Build Discord embed ────────────────────────────────────────────────────
-    payload = {
-        "embeds": [{
-            "title": title,
-            "url":   f"https://github.com/projectdiscovery/nuclei-templates/blob/main/{root_path}",
-            "color": colour,
-            "fields": fields,
-            "description": f"```{description}```"
-        }]
-    }
+	if reference:
+		fields.append({"name": "Reference", "value": reference, "inline": False})
 
-    # ── Send ────────────────────────────────────────────────────────────────────
-    notify_discord(payload)
+	root_path = root_path.replace("/workspaces/nuclei-templates/", "") \
+				   .replace("/home/runner/work/nuclei-templates/nuclei-templates/", "")
+	fields.append({"name": "Path", "value": "`" + root_path + "`", "inline": True})
+	
+	if action == "A":
+		title = f"🆕 `{name}`"
+	else:
+		# Temporary Disable Notify Modified Template
+		return
+		# title = f"✏️ `{name}`"
+
+	# ── Build Discord embed ────────────────────────────────────────────────────
+	payload = {
+		"embeds": [{
+			"title": title,
+			"url":   f"https://github.com/projectdiscovery/nuclei-templates/blob/main/{root_path}",
+			"color": colour,
+			"fields": fields,
+			"description": f"```{description}```"
+		}]
+	}
+
+	# ── Send ────────────────────────────────────────────────────────────────────
+	notify_discord(payload)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("Usage: notify.py <state> <template.yaml>")
-    main(sys.argv[1], sys.argv[2])
+	if len(sys.argv) != 3:
+		sys.exit("Usage: notify.py <state> <template.yaml>")
+	main(sys.argv[1], sys.argv[2])
